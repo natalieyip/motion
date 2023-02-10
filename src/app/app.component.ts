2d8a93ba12c56da3f0457f75b8fd9f04b00c5093
Natalie Yip <yip.natalie92@gmail.com>
2023-02-09 19:07:06 -0800

    Remove hardcoded time and put in real time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Observable, timer, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'motion';
+export class AppComponent implements OnInit {
+
+  dateTime: Observable<Date>
+
+  ngOnInit() {
+    this.dateTime = timer(0, 1000).pipe(
+      map(() => {
+        return new Date()
+      })
+    )
+  }
 }
