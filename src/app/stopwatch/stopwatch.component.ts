211da7667fef060ba3fcd6e4778379d195f46401
Natalie Yip <yip.natalie92@gmail.com>
2023-02-11 20:06:11 -0800

    Update stopwatch timer. Fix styles.

--- src/app/stopwatch/stopwatch.component.ts
+++ src/app/stopwatch/stopwatch.component.ts
@@ -7,11 +7,15 @@ import { Component } from '@angular/core';
 })
 
 export class StopwatchComponent {
-  timer: number = 0;
-  isRunning: boolean = false; 
-  timerId: NodeJS.Timeout;
+  private timer: number = 0;
+  private isRunning: boolean = false; 
+  private timerId: NodeJS.Timeout;
+  private readonly MILLISECONDS_IN_MIN: number = 60000
   buttonText: string = TimerButtonText.START;
   TimerButtonText: typeof TimerButtonText = TimerButtonText;
+  minutes: string | number = '00';
+  seconds: string | number  = '00';
+  milliseconds: string | number = '00';
 
   onClickTimer(): void {
     if (this.isRunning) {
@@ -25,12 +29,33 @@ export class StopwatchComponent {
       this.timer = Date.now() - startTime;
       this.buttonText = TimerButtonText.STOP;
       this.isRunning = true;
+      this.minutes = Math.floor(this.timer / this.MILLISECONDS_IN_MIN);
+      this.seconds = Math.floor((this.timer % this.MILLISECONDS_IN_MIN) / 1000);
+      this.milliseconds = Math.floor((this.timer % 1000) / 10);
+      this.appendZeroToTimeUnit();
     });
 
   }
 
+  private appendZeroToTimeUnit() {
+    if (+this.minutes < 10) {
+      this.minutes = `0${this.minutes}`;
+    }
+
+    if (+this.seconds < 10) {
+      this.seconds = `0${this.seconds}`;
+    }
+          
+    if (+this.milliseconds < 10) {
+      this.milliseconds = `0${this.milliseconds}`;
+    } 
+  }
+
   resetTimer(): void {
     this.timer = 0;
+    this.minutes = '00';
+    this.seconds = '00';
+    this.milliseconds = '00';
   }
 }
 
